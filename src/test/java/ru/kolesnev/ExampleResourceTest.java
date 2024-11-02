510987cfdf29b4401c6732645efa2bd892c52431
Darklight1985 <darklight1985@mail.ru>
2024-11-02 21:09:16 +0300

    Правки

--- src/test/java/ru/kolesnev/ExampleResourceTest.java
+++ src/test/java/ru/kolesnev/ExampleResourceTest.java
@@ -1,9 +1,6 @@
 package ru.kolesnev;
 
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.TestProfile;
-import jakarta.annotation.Resource;
 
 @QuarkusTest
 public class ExampleResourceTest {
